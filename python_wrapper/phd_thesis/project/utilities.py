8aa3333dd0443e853a70d9ee2bec5c1b44328a3d
Federico Tesser <uncleTes@gmail.com>
2016-10-12 10:22:01 +0200

    Removed useless functions from file "project/utilities.py"

--- python_wrapper/phd_thesis/project/utilities.py
+++ python_wrapper/phd_thesis/project/utilities.py
@@ -128,21 +128,6 @@ def check_null_logger(logger,
         logger.addHandler(handler)
     return logger
 
-def points_location(point_to_check,
-                    other_point):
-    if ((point_to_check[0] - other_point[0] <= 0) and
-        (point_to_check[1] - other_point[1] >= 0)):
-        return "nordovest"
-    if ((point_to_check[0] - other_point[0] > 0) and
-        (point_to_check[1] - other_point[1] >= 0)):
-        return "nordest"
-    if ((point_to_check[0] - other_point[0] <= 0) and
-        (point_to_check[1] - other_point[1] < 0)):
-        return "sudovest"
-    if ((point_to_check[0] - other_point[0] > 0) and
-        (point_to_check[1] - other_point[1] < 0)):
-        return "sudest"
-
 def log_msg(message , 
             log_file, 
             logger = None):
@@ -219,20 +204,6 @@ def check_octree(octree,
 
     return l_octree
 
-def check_into_circle(point_to_check,
-                      circle_center ,
-                      circle_radius):
-    check = False
-
-    distance2 = math.pow((point_to_check[0] - circle_center[0]) , 2) + \
-                math.pow((point_to_check[1] - circle_center[1]) , 2)
-    distance = math.sqrt(distance2)
-
-    if distance <= circle_radius:
-        check = True
-
-    return check
-
 def is_point_inside_polygons(point   ,
                              polygons,
                              logger  ,
@@ -291,115 +262,6 @@ def is_point_inside_polygon(point   ,
 
     return inside
 
-def check_point_into_square(point_to_check,
-                            # [x_anchor, x_anchor + edge, 
-                            #  y_anchor, y_anchor + edge]
-                            square        ,
-                            threshold     ,
-                            logger        ,
-                            log_file):
-    check = False
-
-    if isinstance(square, list):
-        if ((point_to_check[0] - square[0] >= threshold) and
-            (square[1] - point_to_check[0] >= threshold) and
-            (point_to_check[1] - square[2] >= threshold) and
-            (square[3] - point_to_check[1] >= threshold)):
-            check = True
-    else:
-        logger = check_null_logger(logger, 
-                                   log_file)
-        logger.error("Second parameter must be a list.")
-    return check
-
-def check_oct_into_square(oct_center,
-                          square    ,
-                          oct_edge  ,
-                          threshold ,
-                          logger    ,
-                          log_file):
-    check = False
-    points_to_check = []
-    an00 = (oct_center[0] - (oct_edge / 2),
-            oct_center[1] - (oct_edge / 2))
-    an10 = (oct_center[0] + (oct_edge / 2),
-            oct_center[1] - (oct_edge / 2))
-    an01 = (oct_center[0] - (oct_edge / 2),
-            oct_center[1] + (oct_edge / 2))
-    an11 = (oct_center[0] + (oct_edge / 2),
-            oct_center[1] + (oct_edge / 2))
-    points_to_check.append(an00)
-    points_to_check.append(an10)
-    points_to_check.append(an01)
-    points_to_check.append(an11)
-
-    for i, point in enumerate(points_to_check):
-        check = check_point_into_square(point    ,
-                                        square   ,
-                                        threshold,
-                                        logger   ,
-                                        log_file)
-        if not check:
-            break
-
-    return check
-
-def check_oct_into_squares(oct_center,
-                           squares   ,
-                           oct_edge  ,
-                           threshold ,
-                           logger    ,
-                           log_file):
-    check = False
-    points_to_check = []
-    an00 = (oct_center[0] - (oct_edge / 2),
-            oct_center[1] - (oct_edge / 2))
-    an10 = (oct_center[0] + (oct_edge / 2),
-            oct_center[1] - (oct_edge / 2))
-    an01 = (oct_center[0] - (oct_edge / 2),
-            oct_center[1] + (oct_edge / 2))
-    an11 = (oct_center[0] + (oct_edge / 2),
-            oct_center[1] + (oct_edge / 2))
-    points_to_check.append(an00)
-    points_to_check.append(an10)
-    points_to_check.append(an01)
-    points_to_check.append(an11)
-
-    for i, point in enumerate(points_to_check):
-        check = check_point_into_squares(point    ,
-                                         squares  ,
-                                         threshold,
-                                         logger   ,
-                                         log_file)
-        if not check:
-            break
-
-    return check
-
-def check_point_into_squares(point_to_check, 
-                             # [[x_anchor, x_anchor + edge, 
-                             #   y_anchor, y_anchor + edge]...]
-                             squares       ,
-                             threshold     ,
-                             logger        ,
-                             log_file):
-    check = False
-
-    if isinstance(squares, list):
-        for i, square in enumerate(squares):
-            check = check_point_into_square(point_to_check,
-                                            square        ,
-                                            threshold     ,
-                                            logger        ,
-                                            log_file)
-            if check:
-                return check
-    else:
-        logger = check_null_logger(logger, 
-                                   log_file)
-        logger.error("Second parameter must be a list of lists.")
-    return check
-
 # https://it.wikipedia.org/wiki/Metodo_dei_minimi_quadrati
 def least_squares(points,
                   unknown_point):
@@ -429,76 +291,6 @@ def least_squares(points,
     coeffs = numpy.sum(p, axis = 0)
     return coeffs
     
-def bil_coeffs(unknown_point, 
-               points_coordinates):
-    coeff_01 = ((points_coordinates[3][0] - unknown_point[0]) * 
-                (points_coordinates[3][1] - unknown_point[1]))
-
-    coeff_02 = ((unknown_point[0] - points_coordinates[0][0]) *
-                (points_coordinates[3][1] - unknown_point[1]))
-
-    coeff_03 = ((points_coordinates[3][0] - unknown_point[0]) *
-                (unknown_point[1] - points_coordinates[0][1]))
-
-    coeff_04 = ((unknown_point[0] - points_coordinates[0][0]) *
-                (unknown_point[1] - points_coordinates[0][1]))
-
-    multiplier = 1 / ((points_coordinates[3][0] - points_coordinates[0][0]) * 
-                      (points_coordinates[3][1] - points_coordinates[0][1]))
-
-    coefficients = [coeff_01, coeff_02, coeff_03, coeff_04]
-
-    coefficients = [coefficient * multiplier for coefficient in coefficients]
-
-    return coefficients
-
-# http://en.wikipedia.org/wiki/Bilinear_interpolation
-#   Q12------------Q22
-#      |          |
-#      |          |
-#      |          |
-#      |          |
-#      |      x,y |
-#   Q11-----------Q21
-#   Q11 = point_values at x1 and y1
-#   Q12 = point_values at x1 and y2
-#   Q21 = point_values at x2 and y1
-#   Q22 = point_values at x2 and y2
-#   f(Q11) = value of the function in x1 and y1
-#   f(Q12) = value of the function in x1 and y2
-#   f(Q21) = value of the function in x2 and y1
-#   f(Q22) = value of the function in x2 and y2
-#   x,y = unknown_point ("unknown point" stand for a point for which it is 
-#         not known the value of the function f)
-def bil_interp(unknown_point	 , 
-               points_coordinates,
-               f_values):
-    coeff_01 = ((points_coordinates[3][0] - unknown_point[0]) * 
-                (points_coordinates[3][1] - unknown_point[1]))
-
-    coeff_02 = ((unknown_point[0] - points_coordinates[0][0]) *
-                (points_coordinates[3][1] - unknown_point[1]))
-
-    coeff_03 = ((points_coordinates[3][0] - unknown_point[0]) *
-                (unknown_point[1] - points_coordinates[0][1]))
-
-    coeff_04 = ((unknown_point[0] - points_coordinates[0][0]) *
-                (unknown_point[1] - points_coordinates[0][1]))
-
-    multiplier = 1 / ((points_coordinates[3][0] - points_coordinates[0][0]) * 
-                      (points_coordinates[3][1] - points_coordinates[0][1]))
-
-    coefficients = [coeff_01, coeff_02, coeff_03, coeff_04]
-
-    coefficients = [coefficient * multiplier for coefficient in coefficients]
-    
-    value = 0
-
-    for i, coeff in enumerate(coefficients):
-        value += (coeff * point_f_values[i])
-
-    return value
-
 # Perspective transformation coefficients (linear coefficients).
 def p_t_coeffs(dim               ,
                original_points   ,
@@ -720,62 +512,6 @@ def metric_coefficients(dimension          ,
 
     return n_m_cs
        
-# Homogeneous coordinate w'.
-def h_c_w_first(dimension   ,
-                in_points   ,
-                coefficients,
-                logger      ,
-                log_file):
-    logger = check_null_logger(logger, log_file)
-    dim = dimension
-    # http://stackoverflow.com/questions/12569452/how-to-identify-numpy-types-in-python
-    if (type(in_points).__module__ == numpy.__name__):
-        points = in_points
-    else: 
-        points = numpy.array(in_points)
-    x_s = numpy.multiply(points[:, 0], coefficients[0][dim])
-    y_s = numpy.multiply(points[:, 1], coefficients[1][dim])
-    if (dim == 3):
-        z_s = numpy.multiply(points[:, 2], coefficients[2][dim])
-    # Inverse of w's (made to be done for vectors).
-    i_w_s_first = numpy.add(x_s, y_s)
-    if (dim == 3):
-        i_w_s_first = numpy.add(i_w_s_first, z_s)
-    i_w_s_first = numpy.add(i_w_s_first, coefficients[dim][dim])
-    w_s_first = numpy.true_divide(1.0, i_w_s_first)
-    if (w_s_first.shape[0] == 1):
-        return w_s_first[0]
-    else:
-        return w_s_first
-
-def compute_w_first(logger,
-                    point,
-                    coefficients):
-    w_first = None
-    dim = len(point)
-    ad_matrix = coefficients
-
-    try:
-        assert (2 <= dim <= 3), "Wrong size for the array passed as point."
-        if (dim == 2):
-            xy = point
-            w_first = 1.0 / ((ad_matrix[0][2] * xy[0]) + \
-                           (ad_matrix[1][2] * xy[1]) + \
-                           ad_matrix[2][2])
-        else:
-            xyz = point
-            w_first = 1.0 / ((ad_matrix[0][3] * xyz[0]) + \
-                           (ad_matrix[1][3] * xyz[1]) + \
-                           (ad_matrix[2][3] * xyz[2]) + \
-                           ad_matrix[3][3])
-
-    except AssertionError:
-        msg_err = sys.exc_info()[1] 
-        logger.error(msg_err)
-    finally:
-        return w_first 
-
-
 def apply_persp_trans_inv(dimension   ,
                           point       ,
                           coefficients,
