348460a79519069bb26c6a9be33c55e2d5d8fb7d
Tex <uncleTes@gmail.com>
2017-07-07 15:14:15 +0200

    Added alias and commands for single core launch in file "auto_incr_refs.sh"

--- python_wrapper/phd_thesis/auto_incr_refs.sh
+++ python_wrapper/phd_thesis/auto_incr_refs.sh
@@ -2,6 +2,7 @@ shopt -s expand_aliases
 #!/bin/bash
 
 # https://unix.stackexchange.com/questions/1496/why-doesnt-my-bash-script-recognize-aliases
+alias mpi1run='/usr/local/lib/openmpi-2.1.1/bin/mpirun -n 1'
 alias mpi2run='/usr/local/lib/openmpi-2.1.1/bin/mpirun -n 2'
 alias mpi4run='/usr/local/lib/openmpi-2.1.1/bin/mpirun -n 4'
 alias mpi6run='/usr/local/lib/openmpi-2.1.1/bin/mpirun -n 6'
@@ -14,10 +15,12 @@ END=6
 
 for ((i=START; i<=END; i++));
 do
+#    mpi1run python project/main.py
     mpi2run python project/main.py
     if (( i < END ));
     then
         sed -i "30s/$var1, $var2/$((var1+incr)), $((var2+incr))/" ./config/PABLO.ini
+#        sed -i "30s/$var1/$((var1+incr))/" ./config/PABLO.ini
     fi
     var1=$((var1+incr))
     var2=$((var2+incr))
